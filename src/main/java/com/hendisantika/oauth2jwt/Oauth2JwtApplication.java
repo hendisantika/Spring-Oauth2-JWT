39ce924c29b5859b31e33f082592c83abfd70e39
Hendi Santika <hendisantika@gmail.com>
2018-07-07 05:27:20 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/oauth2jwt/Oauth2JwtApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.oauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtApplication.class, args);
+    }
+}
