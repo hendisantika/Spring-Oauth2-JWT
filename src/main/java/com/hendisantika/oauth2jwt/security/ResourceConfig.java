50ddfb43439014578a54de03a7ee4c6726c2c742
Hendi Santika <hendisantika@gmail.com>
2018-07-08 06:48:14 +0700

    #10 - Add Resource Config

---
+++ src/main/java/com/hendisantika/oauth2jwt/security/ResourceConfig.java
@@ -0,0 +1,72 @@
+package com.hendisantika.oauth2jwt.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : oauth2-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/07/18
+ * Time: 06.47
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    @Autowired
+    private DefaultTokenServices tokenServices;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources
+                .resourceId(resourceId)
+                .tokenServices(tokenServices)
+                .tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(new OAuthRequestedMatcher())
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/api/hello").access("hasAnyRole('USER')")
+                .antMatchers("/api/me").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/register").hasAuthority("ROLE_REGISTER");
+    }
+
+    private static class OAuthRequestedMatcher implements RequestMatcher {
+        public boolean matches(HttpServletRequest request) {
+            String auth = request.getHeader("Authorization");
+            // Determine if the client request contained an OAuth Authorization
+            boolean haveOauth2Token = (auth != null) && auth.startsWith("Bearer");
+            boolean haveAccessToken = request.getParameter("access_token") != null;
+            return haveOauth2Token || haveAccessToken;
+        }
+    }
+
+
+}
